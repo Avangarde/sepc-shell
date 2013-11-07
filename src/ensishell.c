8af2c7962418a0f48f197d0038250e8b532a98a9
corteshs <corteshs@ensiarchi13.imag.fr>
2013-11-07 15:41:34 +0100

    Arreglados per,isos magicos... ;)

--- src/ensishell.c
+++ src/ensishell.c
@@ -35,6 +35,7 @@ void lancerCommande (struct cmdline * cmdl) {
 	int s;
 	for (s = 0; cmdl->seq[s]!=0;s++){
 		char **commande = cmdl->seq[s];
+   	//     printf("commande: %s\n",commande[0]);
 		if(strcmp(commande[0],"quit") == 0
 		   || strcmp(commande[0],"exit") == 0){
 			exit(0);
@@ -56,8 +57,10 @@ void lancerCommande (struct cmdline * cmdl) {
 					if(s==0){
 					
 					//Est-ce que je suis l'unique sequence
+			//			    printf("Soy la pri,era secuencia\n");
 					
 						if(cmdl->in != NULL){
+				//			    printf("Hay algo en la enrada\n");
 							//Traitement
 							//J'essai d'ouvrir le standard in en mode read-only
 							int stdIn;
@@ -75,11 +78,12 @@ void lancerCommande (struct cmdline * cmdl) {
 						}
 						
 						//Ici on doit regarder si on est l'unique commande ou s'il ya plusieurs
-						if(cmdl->seq[s+1 == 0]){
-							
+						if(cmdl->seq[s+1] == 0){
+					//	    printf("Soy la unicq secuencia\n");							
 							if(cmdl->out != NULL){
+					//								    printf("Hay algo en la salida\n");							
 								int stdOut;
-								if((stdOut = open(cmdl->out, O_RDWR | O_CREAT))==-1){
+								if((stdOut = open(cmdl->out, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO))==-1){
 									perror("open:");
 									exit(1);
 								}
@@ -92,11 +96,14 @@ void lancerCommande (struct cmdline * cmdl) {
 							
 							
 						}else{
+						
+						//    printf("Ordenqr la ejecucion en pipes\n");							
 					
 						dup2(tuyau[1],1);
 						//Attention, est qu'il faut fermer l'ecriture avant de fermer la lecture?
-						close(tuyau[0]);
+
 						close(tuyau[1]);
+						close(tuyau[0]);
 						
 						
 						
@@ -107,16 +114,18 @@ void lancerCommande (struct cmdline * cmdl) {
 					//Est-ce que je suis la deuxieme sequence?
 				
 					if(s==1){
+						//    printf("Soy la segunda secuencia\n");
 					
 						dup2(tuyau[0],0);
-						close(tuyau[0]);
+
 						close(tuyau[1]);
-					
+						close(tuyau[0]);					
 						//Gestion du fichier	out
 						
 						if(cmdl->out != NULL){
+						//      printf("hay algo en la salida\n");
 								int stdOut;
-								if((stdOut = open(cmdl->out, O_RDWR | O_CREAT))==-1){
+								if((stdOut = open(cmdl->out, O_RDWR | O_CREAT, S_IRWXU | S_IRWXG | S_IRWXO))==-1){
 									perror("open:");
 									exit(1);
 								}
@@ -134,11 +143,12 @@ void lancerCommande (struct cmdline * cmdl) {
 					break;
 				default:
 				  { 
+                    	//printf("Modo back\n");
 					int status;
 					if (!cmdl->bg){
 						waitpid(pid, &status, 0);
 					}else{
-						printf("Processus en tache de fond: %i\n", pid);
+						//	printf("Processus en tache de fond: %i\n", pid);
 						processus = ajouterAuFond(pid, cmdl->seq[s], processus);
 					}
 					break;
@@ -154,12 +164,24 @@ void lancerCommande (struct cmdline * cmdl) {
 		
 }
 
+void montrerPipe(struct cmdline * cmdl){
+	int i, j;
+    for (i=0; cmdl->seq[i]!=0; i++) {
+    			char **cmd = cmdl->seq[i];
+    			printf("seq[%d]: ", i);
+                           for (j=0; cmd[j]!=0; j++) {
+                                   printf("'%s' ", cmd[j]);
+                           }
+    			printf("\n");
+    		}
+    
+}
+
 int main() {
         printf("Variante %d: %s\n", VARIANTE, VARIANTE_STRING);
 
 	while (1) {
 		struct cmdline *l;
-		int i, j;
 		char *prompt = "ensishell>";
 
 		l = readcmd(prompt);
@@ -181,14 +203,8 @@ int main() {
 		if (l->bg) printf("background (&)\n");
 
 		/* Display each command of the pipe */
-		for (i=0; l->seq[i]!=0; i++) {
-			char **cmd = l->seq[i];
-			printf("seq[%d]: ", i);
-                        for (j=0; cmd[j]!=0; j++) {
-                                printf("'%s' ", cmd[j]);
-                        }
-			printf("\n");
-		}
+		montrerPipe(l);
+		
 		
 		//lancerCommande(l->seq, l->bg);
 		lancerCommande(l);
